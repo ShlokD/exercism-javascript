ba2ce843a6ab32964f2b44c1b4a7e815b020aa8e
ShlokD <shlokpdesai@gmail.com>
2017-05-29 20:39:19 -0700

    Bob Solution

---
+++ bob/bob.js
@@ -0,0 +1,13 @@
+var Bob = function() {}
+
+Bob.prototype.hey = function(greeting){
+  if(greeting === greeting.toUpperCase() && /[A-Z]/.test(greeting)) {
+    return 'Whoa, chill out!'
+  } else if (greeting.substr(-1) === '?') {
+    return 'Sure.'
+  } else if (/^\s*$/.test(greeting)) {
+    return 'Fine. Be that way!'
+  } else return 'Whatever.'
+}
+
+module.exports = Bob;
