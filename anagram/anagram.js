b5cb99bb54d200a4c1548cde6c7a3d1cc76488fd
ShlokD <shlokpdesai@gmail.com>
2017-06-03 23:52:01 -0700

    Anagram problem

---
+++ anagram/anagram.js
@@ -0,0 +1,46 @@
+var Anagram = function(word) {
+  this.word = word;
+}
+
+Anagram.prototype.matchOne = function(checkWord) {
+  if(checkWord.length !== this.word.length || checkWord.toLowerCase() === this.word.toLowerCase()) {
+    return false
+  }
+
+  var letters = this.word.toLowerCase().split('');
+  var letterMap = {};
+  var returnVal = true;
+  letters.forEach(function(letter) {
+    if(letterMap.hasOwnProperty(letter)) {
+      letterMap[letter]++;
+    } else {
+      letterMap[letter] = 1
+    }
+  });
+
+  var checkLetters = checkWord.toLowerCase().split('');
+
+  checkLetters.forEach(function(checkLetter) {
+    if(letterMap.hasOwnProperty(checkLetter)) {
+      letterMap[checkLetter]--;
+      if(letterMap[checkLetter] === 0) {
+        delete letterMap[checkLetter]
+      }
+    } else {
+      returnVal = false;
+    }
+  })
+
+  return returnVal;
+
+}
+
+Anagram.prototype.matches = function() {
+  var wordList = [].concat.apply([], Array.prototype.slice.call(arguments));
+  var self = this;
+  return wordList.filter(function(word) {
+    return self.matchOne(word)
+  });
+}
+
+module.exports = Anagram;
