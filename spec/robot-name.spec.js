8de85f6891ad6d270a7f67358bcda6096c1cac06
ShlokD <shlokpdesai@gmail.com>
2017-06-07 19:33:06 -0700

    Robot

---
+++ spec/robot-name.spec.js
@@ -0,0 +1,51 @@
+var Robot = require('../robot-name/robot-name.js');
+
+describe('Robot', function() {
+  // NOTE: The 'beforeEach()' and 'afterEach()' act as setup/teardown for this
+  // test suite. See more: http://jasmine.github.io/2.0/introduction.html
+  var robot;
+
+  beforeEach(function() {
+    robot = new Robot();
+  });
+
+  afterEach(function() {
+    robot = null;
+  });
+
+  it('has a name', function() {
+    expect(robot.name).toMatch(/^[A-Z]{2}\d{3}$/);
+  });
+
+  it('name is the same each time', function() {
+    expect(robot.name).toEqual(robot.name);
+  });
+
+  it('different robots have different names', function() {
+    var differentRobot = new Robot();
+    expect(differentRobot.name).not.toEqual(robot.name);
+  });
+
+  it('is able to reset the name', function() {
+    var originalName = robot.name;
+    robot.reset();
+    var newName = robot.name;
+    expect(newName).toMatch(/^[A-Z]{2}\d{3}$/);
+    expect(originalName).not.toEqual(newName);
+  });
+
+  it('should set a unique name after reset', function() {
+    var i,
+        numResets = 10000,
+        usedNames = {};
+
+    usedNames[robot.name] = true;
+
+    for (i = 0; i < numResets; i++) {
+      robot.reset();
+      usedNames[robot.name] = true;
+    }
+
+    expect(Object.keys(usedNames).length).toEqual(numResets + 1);
+  });
+});
