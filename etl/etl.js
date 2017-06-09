bdd056505c4c9974545f3c3e358b1144bcf188be
ShlokD <shlokpdesai@gmail.com>
2017-06-08 17:49:25 -0700

    ETL

---
+++ etl/etl.js
@@ -0,0 +1,14 @@
+class ETL {
+  transform(pointsToLetterMap) {
+    const letterToPointsMap = {};
+    Object.keys(pointsToLetterMap).forEach((point) => {
+      const letters = pointsToLetterMap[point]
+      letters.forEach((letter) => {
+        letterToPointsMap[letter.toLowerCase()] = parseInt(point)
+      })
+    });
+    return letterToPointsMap;
+  }
+}
+
+module.exports = ETL;
