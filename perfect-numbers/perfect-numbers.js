919fb20165b0bf20c9b94a1194e12cf67ae80d9d
ShlokD <shlokpdesai@gmail.com>
2017-06-13 21:41:39 -0700

    Perfect numbers

---
+++ perfect-numbers/perfect-numbers.js
@@ -0,0 +1,32 @@
+class PerfectNumbers  {
+  factorize(number) {
+    const factors = [];
+
+    for(let i = 1; i < number; ++i) {
+      if (number % i === 0) {
+        factors.push(i);
+      }
+    }
+
+    return factors;
+  }
+
+  classify(num) {
+    if(num > 0) {
+      const factors = this.factorize(num);
+      const sum = factors.reduce((factor, acc) => acc + factor, 0);
+      if ( sum === num ) {
+        return "perfect"
+      } else if (sum > num) {
+        return "abundant"
+      }
+
+      return "deficient";
+    }
+
+    return "Classification is only possible for natural numbers.";
+
+  }
+}
+
+module.exports = PerfectNumbers;
