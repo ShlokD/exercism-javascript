f73446e46c88fb36f2b0be7304381301a18fccf3
ShlokD <shlokpdesai@gmail.com>
2017-06-23 16:10:57 -0700

    Raindrops

---
+++ raindrops/raindrops.js
@@ -0,0 +1,22 @@
+class Raindrops {
+  convert(num) {
+    let raindrop  = "";
+    if(num % 3 === 0) {
+      raindrop = raindrop.concat("Pling");
+    }
+    if(num % 5 === 0) {
+      raindrop = raindrop.concat("Plang");
+    }
+    if(num % 7 === 0) {
+      raindrop = raindrop.concat("Plong");
+    }
+
+    if(raindrop.length === 0) {
+      raindrop = num.toString()
+    }
+
+    return raindrop;
+  }
+}
+
+module.exports = Raindrops;
