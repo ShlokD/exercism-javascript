acdd7dae91b99205f1cdb1fb6970088cc2711d9c
ShlokD <shlokpdesai@gmail.com>
2017-06-28 19:11:07 -0700

    Cryptosquare

---
+++ crypto-square/crypto-square.js
@@ -0,0 +1,32 @@
+class Crypto {
+  constructor(text) {
+    this.text = text;
+  }
+
+  normalizePlaintext() {
+    return this.text.toLowerCase().replace(/[^a-z0-9]/g, '');
+  }
+
+  size() {
+    return Math.ceil(Math.sqrt(this.normalizePlaintext().length));
+  }
+
+  plaintextSegments() {
+    const size = this.size();
+    return this.normalizePlaintext().match(new RegExp(`.{1,${size}}`, 'g'));
+  }
+
+  ciphertext() {
+    const chunks = this.plaintextSegments();
+    const size = this.size();
+    let cipherText = "";
+    for(let i = 0; i < chunks.length; ++i) {
+      for ( let j = 0; j < size; ++j ) {
+        cipherText = cipherText.concat(chunks[j][i] || "")
+      }
+    }
+    return cipherText;
+  }
+}
+
+module.exports = Crypto;
