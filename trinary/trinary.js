e4bacda72ffa83c312ff8502e850d9afc23524e5
ShlokD <shlokpdesai@gmail.com>
2017-06-29 19:33:50 -0700

    Trinary

---
+++ trinary/trinary.js
@@ -0,0 +1,16 @@
+class Trinary {
+  constructor(triNum) {
+    this.num = triNum;
+    this.isValid = /^[012]+$/.test(triNum);
+  }
+
+  toDecimal() {
+    if(this.isValid) {
+      return this.num.split('').reverse().reduce((total, digit, index) => total += Math.pow(3, index) * digit, 0)
+    }
+    return 0;
+  }
+
+}
+
+module.exports = Trinary;
